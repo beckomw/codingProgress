264b9a7806646c8c8ee09e72e644a67805cef910
Wayne Beckom <Waynebeckom@gmail.com>
2023-11-02 02:09:46 -0600

    folder switch

---
+++ basics/scope-2.js
@@ -0,0 +1,24 @@
+// Global (name)
+    // Local (name)
+        // Local
+
+    // Local
+
+//let name = 'Wayne'
+
+
+
+
+
+if (true) {
+    //let name = 'Mike'
+
+    if (true) {
+       let name = 'Jen'
+        console.log(name)
+    }
+}
+
+if (true) {
+    console.log(name)
+}
