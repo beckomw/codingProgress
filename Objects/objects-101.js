d131e0c3e1a4478fe02bbeb52af1d222082f6c60
Wayne Beckom <Waynebeckom@gmail.com>
2023-11-05 19:33:51 -0700

    playing with objects

---
+++ Objects/objects-101.js
@@ -0,0 +1,29 @@
+let myBook = {
+    title: '1984',
+    author: 'George Orwell',
+    pageCount: 326
+}
+
+
+
+console.log(myBook.title)
+ 
+
+// Challenge area 
+// name, age, location 
+
+
+let person1 = {
+    name: 'wayne',
+    age: 31, 
+    location: 'Denver' 
+}
+console.log(`${person1.name} is ${person1.age} and lives in ${person1.location}`)
+
+
+
+person1.age += 1;
+
+
+
+console.log(`${person1.name} is ${person1.age} and lives in ${person1.location}`)
