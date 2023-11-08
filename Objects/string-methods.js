f1ca114a95164c649dc27b14a0b556e9e024ec35
Wayne Beckom <Waynebeckom@gmail.com>
2023-11-08 00:57:34 -0700

    new methods

---
+++ Objects/string-methods.js
@@ -0,0 +1,18 @@
+let name = 'Wayne Beckom'
+
+// Length property 
+console.log(name.length)
+
+
+// Convert to upper case 
+console.log(name.toUpperCase())
+
+// Convert to lower case 
+console.log(name.toLowerCase())
+
+// 
+
+let password = 'abce123098'
+console.log(password.includes('password'))
+
+
