40f94920b9acf1f37d07931717a983376bfa069b
Wayne Beckom <Waynebeckom@gmail.com>
2024-01-04 21:55:08 -0700

    index OF

--- Arrays/notes.js
+++ Arrays/notes.js
@@ -1,4 +1,13 @@
-const notes = ['Note 1', 'Note 2','Note 3']
+const notes = [{
+    title: 'My next trip',
+    body: 'I would like to go to Spain',
+},{
+    title:'Habits to work one',
+    body: 'Exercise. Eating a bit better'
+},{
+    title:'Office modification',
+    body: 'Get a new job'
+}]
 
 
 // console.log(notes.pop())
@@ -12,20 +21,24 @@ const notes = ['Note 1', 'Note 2','Note 3']
 // notes.splice(1, 0, 'This is the new second item')
 
 
-notes[2] = 'This is now the new note 3'
+// notes[2] = 'This is now the new note 3'
 
 
-notes.forEach(function(item, index){
-    console.log(index)
-    console.log(item)
-})
+// notes.forEach(function(item, index){
+//     console.log(index)
+//     console.log(item)
+// })
 
-////////////
+// ////////////
+// console.log(notes.length)
+// console.log(notes)
+
+
+
+// // Counting... 1 
+
+
 console.log(notes.length)
 console.log(notes)
 
-
-
-// Counting... 1 
-
-
+console.log(notes.indexOf({}))
