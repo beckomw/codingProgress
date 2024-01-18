56f91a66f40de916be8845e74876692a20069b83
Wayne Beckom <Waynebeckom@gmail.com>
2024-01-18 04:31:42 -0700

    Java switch no longer using JavaScript

--- first.java
+++ first.java
@@ -1,10 +1,15 @@
+import java.util.Scanner; 
+import java.lang.*;
 
-
-class First 
+public class First 
 {
     public static void main(String args[])
     {
         System.out.println("Hello World");
+	Scanner s=new Scanner(); 
+	int a,b,c; 
+	System.out.println("Enter 2 numbers")
+
 
     }
 
